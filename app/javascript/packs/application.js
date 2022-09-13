a769439421ed16f75b130f2836d531400aaa4032
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-09-13 10:03:28 +0000

    [Finish] bookers15

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+//= require jquery
+//= require jquery3
+//= require jquery_ujs
+
 
 Rails.start()
 Turbolinks.start()
