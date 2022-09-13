dbf7850d06c93a414fa00cd9c1e061b508280398
KittaMiura <112059987+roppou@users.noreply.github.com>
2022-09-13 16:48:35 +0000

    [Success] validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id), notice: "Book was successfully created."
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash[:alert] = "<%= @book.errors.count %> errors prohibited this book from being saved:"
+      render 'index'
+    end
   end
 
   def show
@@ -24,13 +30,15 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id), notice: "Book was successfully updated."
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
   end
 
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to '/books', notice: "Book was successfully destroyed."
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
   end
 
   private
